80de2944b99dc99ab5bfc1ad3e2bf1fe858bde6e
Maksim <kodermax@gmail.com>
2023-03-07 19:08:49 +0300

    up

--- apps/admin/src/@core/layouts/components/shared-components/footer/FooterContent.tsx
+++ apps/admin/src/@core/layouts/components/shared-components/footer/FooterContent.tsx
@@ -1,14 +1,7 @@
-// ** MUI Imports
 import Box from '@mui/material/Box'
-import Link from '@mui/material/Link'
-import { Theme } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
-import useMediaQuery from '@mui/material/useMediaQuery'
 
 const FooterContent = () => {
-  // ** Var
-  const hidden = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'))
-
   return (
     <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
       <Typography sx={{ mr: 2 }}>
