ca6af47bd0a8dae39c00348a4cddb3c686ca13e0
Maksim Karpychev <kodermax@gmail.com>
2023-03-09 00:53:56 +0300

    up

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true
-}
+})
