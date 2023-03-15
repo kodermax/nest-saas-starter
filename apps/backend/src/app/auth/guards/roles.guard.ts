ccfa0ee0792c01bd7726c3fe1bdd0437b0fb4ca3
Maksim Karpychev <kodermax@gmail.com>
2023-03-15 23:45:00 +0300

    up

--- apps/backend/src/app/auth/guards/roles.guard.ts
+++ apps/backend/src/app/auth/guards/roles.guard.ts
@@ -16,7 +16,6 @@ export class RolesGuard implements CanActivate {
             return true;
         }
         const { user } = context.switchToHttp().getRequest();
-        console.log(user)
         return requiredRoles.some((role) => user.roles?.includes(role));
     }
 }
