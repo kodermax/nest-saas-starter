f140c498839918b9e246e43896465288835f652e
Maksim Karpychev <kodermax@gmail.com>
2023-03-20 00:11:22 +0300

    up

---
+++ apps/admin/src/pages/apps/books/Token.tsx
@@ -0,0 +1,61 @@
+import { styled } from '@mui/material'
+import { ForwardedRef, ReactNode, forwardRef, useImperativeHandle, useState } from 'react'
+
+interface TokenProps {
+  highlight?: boolean
+}
+
+export interface TokenAdvancedRef {
+  setHightLightItem: () => void
+}
+const StyledSpan = styled('span', { shouldForwardProp: prop => prop !== 'highlight' })<TokenProps>(({ highlight }) => ({
+  cursor: 'pointer',
+  '&:hover': {
+    backgroundColor: '#008c1a',
+    color: 'white',
+    borderRadius: 3
+  },
+  ...(highlight && {
+    backgroundColor: '#008c1a',
+    color: 'white',
+    borderRadius: 3
+  })
+}))
+
+interface Props {
+  id: string
+  matches: string
+  children: ReactNode
+  onMouseEnter: (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void
+  onMouseLeave: (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void
+  word?: string
+}
+
+const Token = (props: Props, ref: ForwardedRef<TokenAdvancedRef>) => {
+  const [highlight, setHighLight] = useState<boolean>(false)
+  useImperativeHandle<TokenAdvancedRef, TokenAdvancedRef>(
+    ref,
+    () => ({
+      setHightLightItem: () => {
+        setHighLight((prev: boolean) => !prev)
+      }
+    }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  )
+
+  return (
+    <StyledSpan
+      id={`word${props.id}`}
+      data-word={props.word}
+      data-matches={props.matches}
+      highlight={highlight}
+      onMouseEnter={props.onMouseEnter}
+      onMouseLeave={props.onMouseLeave}
+    >
+      {props.children}
+    </StyledSpan>
+  )
+}
+
+export default forwardRef<TokenAdvancedRef, Props>(Token)
--- apps/admin/src/pages/apps/books/index.tsx
+++ apps/admin/src/pages/apps/books/index.tsx
@@ -1,7 +1,93 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import data from './page.json'
+import Grid from '@mui/material/Unstable_Grid2'
+import { Paper, styled } from '@mui/material'
+import Token, { TokenAdvancedRef } from './Token'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  minHeight: 200,
+  '& .highlight': {
+    backgroundColor: '#008c1a',
+    color: 'white',
+    borderRadius: 3
+  }
+}))
 
 const Books = () => {
-  return <>TEST</>
+  const refs = useRef<[TokenAdvancedRef | null]>([null])
+  const trRefs = useRef<[TokenAdvancedRef | null]>([null])
+
+  const handleMouseEnter = (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+    const matches = event.currentTarget.getAttribute('data-matches')
+    const hasWord = !!event.currentTarget.getAttribute('data-word')
+    if (matches) {
+      const highlightIds = matches.split(',')
+      for (const id of highlightIds) {
+        hasWord ? trRefs.current[id].setHightLightItem() : refs.current[id].setHightLightItem()
+      }
+    }
+  }
+  const handleMouseLeave = (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+    const matches = event.currentTarget.getAttribute('data-matches')
+    const hasWord = !!event.currentTarget.getAttribute('data-word')
+    if (matches) {
+      const highlightIds = matches.split(',')
+      for (const id of highlightIds) {
+        hasWord ? trRefs.current[id].setHightLightItem() : refs.current[id].setHightLightItem()
+      }
+    }
+  }
+
+  return (
+    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+      <Grid xs={6}>
+        <Item>
+          {data.sentences.map((sentence: any) => {
+            return sentence.pieces.map((piece: any) => {
+              return piece.tokens.map((token: any) => {
+                return (
+                  <Token
+                    key={token.id}
+                    id={token.id}
+                    matches={token.matches}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    word={token.text}
+                    ref={el => (refs.current[token.id] = el)}
+                  >
+                    {token.text + token.after}
+                  </Token>
+                )
+              })
+            })
+          })}
+        </Item>
+      </Grid>
+      <Grid xs={6}>
+        <Item id={'tr0'}>
+          {data.tokens_ru.map((token: any) => {
+            return (
+              <Token
+                id={token.id}
+                key={token.id}
+                matches={token.matches}
+                ref={el => (trRefs.current[token.id] = el)}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                {token.text + token.after}
+              </Token>
+            )
+          })}
+        </Item>
+      </Grid>
+    </Grid>
+  )
 }
 Books.acl = {
   action: 'manage',
