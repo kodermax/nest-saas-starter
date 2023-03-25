4773d8a9cd1dfab38e5638e459327a1607a71153
Maksim <kodermax@gmail.com>
2023-03-25 22:22:21 +0300

    up

--- apps/backend/src/app/prisma/prisma.module.ts
+++ apps/backend/src/app/prisma/prisma.module.ts
@@ -1,9 +1,21 @@
-import { Global, Module } from '@nestjs/common';
+import { FactoryProvider, Global, Module, Scope } from '@nestjs/common';
+import { REQUEST } from '@nestjs/core';
+import { PrismaClient } from '@prisma/client';
 import { PrismaService } from 'src/app/prisma/prisma.service';
+import { PrismaClientManager } from './prisma-client-manager';
+import { Request } from 'express';
+
+const prismaClientProvider: FactoryProvider<PrismaClient> = {
+  provide: PrismaClient,
+  scope: Scope.REQUEST,
+  inject: [REQUEST, PrismaClientManager],
+  useFactory: (request: Request, manager: PrismaClientManager) =>
+    manager.getClient(request),
+};
 
 @Global()
 @Module({
-  providers: [PrismaService],
+  providers: [PrismaService, prismaClientProvider],
   exports: [PrismaService],
 
 })
