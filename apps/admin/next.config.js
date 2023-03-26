ed956caf8ba978738f88dba08b67cce07661bd57
Maksim <kodermax@gmail.com>
2023-03-26 22:57:11 +0300

    up

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
