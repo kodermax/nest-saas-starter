c0505f7a3132b2c1b9bd763bc88916cf945e1c53
Maksim <kodermax@gmail.com>
2023-03-27 12:43:29 +0300

    up

--- apps/error/pages/index.tsx
+++ apps/error/pages/index.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 function HomePage() {
   return (
     <>
-      <h1>Domain error</h1>
-      <h2>Failed to resolve DNS path for this host</h2>
+      <h1>Ощибка домена</h1>
+      <h2>Не удалось разрешить DNS-путь для этого хоста</h2>
     </>
   )
 }
