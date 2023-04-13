bf23cdf7586137aa49fe51a65deeee904a6bdc78
Maksim Karpychev <kodermax@gmail.com>
2023-04-13 19:06:00 +0300

    up

--- apps/backend/prisma/seed.ts
+++ apps/backend/prisma/seed.ts
@@ -1,27 +1,24 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, UserRole } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
 async function main() {
     console.log(`Start seeding ...`)
-    await prisma.$connect();
     await prisma.user.deleteMany();
     await prisma.user.create({
         data: {
             email: 'admin@starter.com',
             firstName: 'Admin',
-            lastName: 'Starter',
             password: '$2b$10$mpRF.dW4GupwVD3apkH1y.oPvdFrmGELxSebXvanmHrU9sI84aYWi',
-            roles: ['Admin']
+            roles: [UserRole.Admin]
         },
     })
     await prisma.user.create({
         data: {
             email: 'client@starter.com',
             firstName: 'Client',
-            lastName: 'Starter',
             password: '$2b$10$oeMKDFLR.PN4obPdCcufY.SNuilb82PS4yzcSO/FEtyqTeymBcrwq',
-            roles: ['User']
+            roles: [UserRole.User]
         },
     })
     console.log(`Seeding finished.`)
--- apps/backend/prisma_mongo/seed.ts
+++ apps/backend/prisma_mongo/seed.ts
@@ -1,24 +1,27 @@
-import { PrismaClient, UserRole } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
 async function main() {
     console.log(`Start seeding ...`)
+    await prisma.$connect();
     await prisma.user.deleteMany();
     await prisma.user.create({
         data: {
             email: 'admin@starter.com',
             firstName: 'Admin',
+            lastName: 'Starter',
             password: '$2b$10$mpRF.dW4GupwVD3apkH1y.oPvdFrmGELxSebXvanmHrU9sI84aYWi',
-            roles: [UserRole.Admin]
+            roles: ['Admin']
         },
     })
     await prisma.user.create({
         data: {
             email: 'client@starter.com',
             firstName: 'Client',
+            lastName: 'Starter',
             password: '$2b$10$oeMKDFLR.PN4obPdCcufY.SNuilb82PS4yzcSO/FEtyqTeymBcrwq',
-            roles: [UserRole.User]
+            roles: ['User']
         },
     })
     console.log(`Seeding finished.`)
