ba306804711a8143f03c6d099570292743e7f251
Maksim <kodermax@gmail.com>
2023-04-16 18:52:58 +0300

    up

--- apps/backend/apps/account/src/controllers/tenants.controller.ts
+++ apps/backend/apps/account/src/controllers/tenants.controller.ts
@@ -8,11 +8,12 @@ import { CreateTenantInput } from '../dto/create-tenant.input';
 import { ApiConflictResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CheckAvailabilityDto } from '../dto/check-availability.dto';
 import { Request, Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @ApiTags('Tenants')
 @Controller('tenants')
 export class TenantsController {
-    constructor(private readonly tenants: TenantsService) { }
+    constructor(private readonly tenants: TenantsService, private readonly config: ConfigService) { }
 
     @Post('check-availability')
     @HttpCode(200)
@@ -27,7 +28,14 @@ export class TenantsController {
     @ApiConflictResponse({ description: 'Такой домен уже существует' })
     async createTenant(@Body() payload: CreateTenantInput, @Res({ passthrough: true }) response: Response) {
         const tenant = await this.tenants.createTenant(payload);
-        response.cookie('tenantId', tenant.id);
+        response.cookie('tenantId', tenant.id, {
+            domain: this.config.get('AUTH_COOKIE_DOMAIN'),
+            maxAge: 3600 * 3600,
+            secure: this.config.get('ENVIRONMENT') !== 'development' ? true : false,
+            path: '/',
+            httpOnly: true,
+            sameSite: 'none',
+        });
         return tenant;
     }
 
