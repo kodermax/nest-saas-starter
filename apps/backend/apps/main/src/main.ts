ce1a11f9db855267ca6ddffe7f9bf23223121e22
Maksim <kodermax@gmail.com>
2023-04-16 23:52:14 +0300

    up

--- apps/backend/apps/main/src/main.ts
+++ apps/backend/apps/main/src/main.ts
@@ -12,7 +12,7 @@ import { PrismaService } from '@app/prisma';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(MainModule);
   app.useGlobalPipes(new ValidationPipe());
-
+  app.setGlobalPrefix('main-api')
   const prismaService: PrismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
